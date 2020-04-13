f90aa67a72415c30ad22b819211fe6486227260f
tig3r4ce <tigerace@gmail.com>
2020-04-13 14:04:18 -0400

    Adding auto-link-section functionality

--- includes/cah-spa-ensemble-cpt-editor.php
+++ includes/cah-spa-ensemble-cpt-editor.php
@@ -21,8 +21,7 @@ if( !class_exists( 'CAH_SPAEnsembleCPTEditor' ) ) {
 
             add_action( 'save_post_ensemble', [ __CLASS__, 'save' ], 10, 0 );
 
-            // Scripts are empty at the moment, so we don't technically need this yet.
-            // add_action( 'admin_enqueue_scripts', [ __CLASS__, 'maybe_load_scripts' ] );
+            add_action( 'admin_enqueue_scripts', [ __CLASS__, 'maybe_load_scripts' ] );
         }
 
 
@@ -40,6 +39,15 @@ if( !class_exists( 'CAH_SPAEnsembleCPTEditor' ) ) {
             //      - the post type to add the box for
             //      - situations to show the box in
             //      - priority for box display
+            add_meta_box(
+                'ensemble_right_menu_links',
+                'Sidebar Menu Links',
+                [ __CLASS__, 'menu_links' ],
+                'ensemble',
+                'normal',
+                'low'
+            );
+
             add_meta_box(
                 'ensemble_sidebar',
                 'Right Sidebar',
@@ -67,6 +75,27 @@ if( !class_exists( 'CAH_SPAEnsembleCPTEditor' ) ) {
             if( isset( $_POST['sidebar-content'] ) ) {
                 update_post_meta( $post->ID, 'spa-ensemble-sidebar-content', $_POST['sidebar-content'] );
             }
+
+            $sections = [];
+
+            if( isset( $_POST['section_names'] ) && !empty( $_POST['section_names'] ) ) {
+                foreach( $_POST['section_names'] as $i => $section ) {
+                    $new_section = [ 'name' => $section, 'links' => [], ];
+
+                    if( isset( $_POST["section_${i}_link_hrefs"] ) ) {
+                        foreach( $_POST["section_${i}_link_hrefs"] as $j => $link ) {
+                            $new_section['links'][] = [
+                                'name' => isset( $_POST["section_${i}_link_names"][$j] ) ? $_POST["section_${i}_link_names"][$j] : '',
+                                'href' => $link,
+                            ];
+                        }
+                    }
+
+                    $sections[] = $new_section;
+                }
+            }
+
+            update_post_meta( $post->ID, 'spa-ensemble-link-sections', $sections );
         }
 
 
@@ -87,6 +116,106 @@ if( !class_exists( 'CAH_SPAEnsembleCPTEditor' ) ) {
         }
 
 
+        public static function menu_links() {
+
+            global $post;
+
+            $sections = maybe_unserialize( get_post_meta( $post->ID, 'spa-ensemble-link-sections', true ) );
+
+            if( !is_array( $sections ) || empty( $sections ) ) {
+                $sections = [
+                    [
+                        'name' => '',
+                        'links' => [
+                            [
+                                'name' => '',
+                                'href' => '',
+                            ],
+                        ],
+                    ],
+                ];
+            }
+
+            ?>
+
+            <div class="inner-meta" id="link-flex-box">
+            <?php foreach( $sections as $i => $section ) : ?>
+                <div class="link-section" id="section-<?= $i ?>">
+                    <div class="section-name">
+                        <div>
+                            <label>Section Name: </label>
+                            <input type="text" 
+                                size="50" 
+                                name="section_names[]" 
+                                id="name-section-<?= $i ?>"
+                                value="<?= isset( $section['name'] ) ? $section['name'] : '' ?>"
+                            >
+                        </div>
+                        <button type="button" class="button button-delete button-delete-section" id="delete-section-<?= $i ?>" aria-label="Delete Section">
+                            <span class="dashicons dashicons-trash" aria-hidden="true"></span>
+                        </button>
+                    </div>
+                    <div class="link-box">
+                    <?php if( isset( $section['links'] ) && !empty( $section['links'] ) ) : ?>
+                        <?php foreach( $section['links'] as $j => $link ) : ?>
+                        <div class="link-entry" id="section-<?= $i ?>-link-<?= $j ?>">
+                            <div class="link-name">
+                                <label>Link Name: </label>
+                                <input type="text" 
+                                    size="30"
+                                    name="section_<?= $i ?>_link_names[]"
+                                    id="label-section-<?= $i ?>-link-<?= $j ?>"
+                                    value="<?= isset( $link['name'] ) ? $link['name'] : '' ?>"
+                                >
+                            </div>
+                            <div class="link-addr">
+                                <label>Link Address: </label>
+                                <input type="text" 
+                                    size="75"
+                                    name="section_<?= $i ?>_link_hrefs[]"
+                                    id="href-section-<?= $i ?>-link-<?= $j ?>"
+                                    value="<?= isset( $link['href'] ) ? $link['href'] : '' ?>"
+                                >
+                            </div>
+                            <div class="link-delete">
+                                <button type="button" 
+                                    class="button button-delete button-delete-link"
+                                    id="delete-section-<?= $i ?>-link-<?= $j ?>"
+                                    aria-label="Delete Link"
+                                >
+                                    <span class="dashicons dashicons-trash" aria-hidden="true"></span>
+                                </button>
+                            </div>
+                        </div>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                        <div>
+                            <button type="button"
+                                class="button button-primary button-add-link"
+                                id="button-add-link-section-<?= $i ?>"
+                                aria-label="Add Link"
+                            >
+                                <span class="dashicons dashicons-plus" aria-hidden="true"></span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+                <div>
+                    <button type="button"
+                            class="button button-primary"
+                            id="button-add-section"
+                            aria-label="Add Section"
+                    >
+                        <span class="dashicons dashicons-plus" aria-hidden="true"></span>
+                    </button>
+                </div>
+            </div>
+
+            <?php
+        }
+
+
         /**
          * Load our admin scripts and styles if we're creating a new ensemble post or
          * editing an existing one.
@@ -103,7 +232,7 @@ if( !class_exists( 'CAH_SPAEnsembleCPTEditor' ) ) {
             ) {
                 wp_enqueue_script( 
                     'cah-spa-ensemble-admin', 
-                    CAH_SPA_ENSEMBLE__PLUGIN_DIR_URL . 'dist/js/admin.min.js', 
+                    CAH_SPA_ENSEMBLE__PLUGIN_DIR_URL . 'src/js/admin.js', 
                     [ 'jquery' ], 
                     CAH_SPA_ENSEMBLE__VERSION, 
                     true
--- src/js/admin.js
+++ src/js/admin.js
@@ -1,1 +1,249 @@
 // Space for admin/editor scripts
+
+(function($) {
+
+    let indices = []
+
+    const findId = function(elem, isLink = false) {
+        const targetStr = 'section-' + (isLink ? '\\d+-link-' : '') + '(\\d+)'
+        const patt = new RegExp(targetStr)
+
+        const matches = $(elem).attr('id').match(patt)
+
+        if (matches) {
+            return parseInt(matches[1])
+        }
+
+        return null
+    }
+
+    const createLink = function(linkId, secId) {
+        const sec = 'section-' + secId
+        const link = 'link-' + linkId
+
+        const newRow = $('<div></div>').attr({
+            class: 'link-entry',
+            id: sec + '-' + link,
+        }).append(
+            $('<div></div>').attr({
+                class: 'link-name',
+            }).append(
+                $('<label></label>').html('Link Name: ')
+            ).append(
+                $('<input></input>').attr({
+                    type: 'text',
+                    size: 30,
+                    name: 'section_' + secId + '_link_names[]',
+                    id: 'label-' + sec + '-' + link,
+                })
+            )
+        ).append(
+            $('<div></div>').attr({
+                class: 'link-addr',
+            }).append(
+                $('<label></label>').html('Link Address: ')
+            ).append(
+                $('<input></input>').attr({
+                    type: 'text',
+                    size: 75,
+                    name: 'section_' + secId + '_link_hrefs[]',
+                    id: 'href-' + sec + '-' + link,
+                })
+            )
+        ).append(
+            $('<div></div>').attr({
+                class: 'link-delete',
+            }).append(
+                $('<button></button>').attr({
+                    type: 'button',
+                    class: 'button button-delete button-delete-link',
+                    id: 'delete-' + sec + '-' + link,
+                    'aria-label': 'Delete Link',
+                }).append(
+                    $('<span></span>').attr({
+                        class: 'dashicons dashicons-trash',
+                        'aria-hidden': true,
+                    })
+                )
+            )
+        )
+
+        indices[secId] = linkId
+
+        return newRow
+    }
+
+
+    const createSection = function(secId) {
+        const newSec = $('<div></div>').attr({
+            class: 'link-section',
+            id: 'section-' + secId,
+        }).append(
+            $('<div></div>').attr({
+                class: 'section-name',
+            }).append(
+                $('<div></div>').append(
+                    $('<label></label>').html('Section Name: ')
+                ).append(
+                    $('<input></input>').attr({
+                        type: 'text',
+                        size: 50,
+                        name: 'section_names[]',
+                        id: 'name-section-' + secId,
+                    })
+                )
+            ).append(
+                $('<button></button>').attr({
+                    type: 'button',
+                    class: 'button button-delete button-delete-section',
+                    id: 'delete-section-' + secId,
+                    'aria-label': 'Delete Section',
+                }).append(
+                    $('<span></span>').attr({
+                        class: 'dashicons dashicons-trash',
+                        'aria-hidden': true,
+                    })
+                )
+            )
+        )
+
+        const linkBox = $('<div></div>').attr({
+            class: 'link-box',
+        })
+
+        const newLink = createLink(0, secId)
+
+        linkBox.append(newLink)
+
+        const linkButton = $('<div></div>').append(
+            $('<button></button>').attr({
+                type: 'button',
+                class: 'button button-primary button-add-link',
+                id: 'button-add-link-section-' + secId,
+                'aria-label': 'Add Link',
+            }).append(
+                $('<span></span>').attr({
+                    class: 'dashicons dashicons-plus',
+                    'aria-hidden': true,
+                })
+            )
+        )
+
+        linkBox.append(linkButton)
+
+        newSec.append(linkBox)
+
+        return newSec
+    }
+
+
+    const findIndices = function() {
+        const sections = $('.link-section')
+
+        $(sections).each(function() {
+            const index = findId(this)
+
+            if (index === null) return
+
+            const links = $(this).find('.link-entry')
+            let linkIndex = findId($(links).last(), true)
+
+            if (linkIndex === null) linkIndex = 0
+
+            indices[index] = linkIndex
+        })
+        
+    }
+
+
+    const addDeleteListeners = function() {
+        $('.button-delete-section').on('click', function(e) {
+            e.preventDefault()
+            e.stopImmediatePropagation()
+
+            const target = findId(this)
+
+            if (target === null) return
+
+            if ($('.link-section').length > 1) {
+                indices[target] = null
+                $('#section-' + target).remove()
+            }
+            else {
+                $('.link-section').find('.button-delete-link').each(function() {
+                    $(this).click()
+                })
+
+                $('#name-section-' + target).val('')
+            }
+        })
+
+        $('.button-delete-link').on('click', function(e) {
+            e.preventDefault()
+            e.stopImmediatePropagation()
+
+            const target = findId(this, true)
+            const section = findId(this)
+
+            if (target === null || section === null) return
+
+            const links = $('#section-' + section).find('.link-entry')
+
+            if (links.length > 1) {
+
+                if ($(this).is($(links).last())) {
+                    indices[section] = findId($(links).last().prev(), true)
+                }
+
+                $('#section-' + section + '-link-' + target).remove()
+            }
+            else {
+                $(links).find('input').each(function() {
+                    $(this).val('')
+                })
+            }
+        })
+    }
+
+
+    const addPlusListeners = function() {
+        $('.button-add-link').on('click', function(e) {
+            e.preventDefault()
+            e.stopImmediatePropagation()
+
+            const secId = findId(this)
+
+            if (secId === null) return
+
+            const section = $('#section-' + secId)
+            const newIndex = indices[secId] + 1
+
+            const newLink = createLink(newIndex, secId)
+
+            $(section).find('.link-box > div:last-child').before(newLink)
+            indices[secId] = newIndex
+            addDeleteListeners()
+        })
+
+        $('#button-add-section').on('click', function(e) {
+            e.preventDefault()
+            e.stopImmediatePropagation()
+
+            const target = findId($('.link-section').last())
+
+            if (target === null) return
+
+            const newSection = createSection(target + 1)
+
+            $(this).before(newSection)
+            addDeleteListeners()
+            addPlusListeners()
+        })
+    }
+
+    $(document).ready(function() {
+        findIndices()
+        addDeleteListeners()
+        addPlusListeners()
+    })
+})(jQuery)
